5c7c25df43b4f1130d2af84506797e27e414a10e
netcec <aw133e@gmail.com>
2015-12-10 10:44:45 +0800

    字符隐藏方法

--- protected/components/Common.php
+++ protected/components/Common.php
@@ -338,5 +338,143 @@ class Common {
         return $week_array;
     }
 
+    /**
+    +----------------------------------------------------------
+     * @use 将一个字符串部分字符用*替代隐藏
+    +----------------------------------------------------------
+     * @param string    $string   待转换的字符串
+     * @param int       $bengin   起始位置，从0开始计数，当$type=4时，表示左侧保留长度
+     * @param int       $len      需要转换成*的字符个数，当$type=4时，表示右侧保留长度
+     * @param int       $type     转换类型：0，从左向右隐藏；1，从右向左隐藏；2，从指定字符位置分割前由右向左隐藏；3，从指定字符位置分割后由左向右隐藏；4，保留首末指定字符串
+     * @param string    $glue     分割符
+    +----------------------------------------------------------
+     * @return string   处理后的字符串
+    +----------------------------------------------------------
+     * 静态方法内部调用的时候，需要使用 parent::method()/self::method()
+     * $this->staticProperty(父类的静态属性不可以通过$this(子类实例)来访问，
+     * 会有这样报错:
+     * PHP Strict Standards: Accessing static property 类名::$country as non static in，
+    +----------------------------------------------------------
+     * @eg.:
+    +----------------------------------------------------------
+     * 从左边第5个字符向右隐藏4个字符：" . hideStr($str0, 4, 4);
+     * 从右边第5个字符向左隐藏4个字符：" . hideStr($str0, 4, 4, 1);
+     * 隐藏指定字符@（或其他字符）前面的从右边第1个字符向左隐藏4个字符：" . hideStr($str2, 0, 4, 2);
+     * 隐藏指定字符@（或其他字符）后面的从左边第1个字符向右隐藏4个字符：" . hideStr($str3, 0, 4, 3);
+     * 隐藏首保留3个字符，右保留2个字符：" . hideStr($str4, 3, 2, 4);
+     */
+    public static function hideStr($string, $bengin = 0, $len = 4, $type = 0, $glue = "@") {
+        if (empty($string))
+            return false;
+        $array = array();
+        if ($type == 0 || $type == 1 || $type == 4) {
+            $strlen = $length = mb_strlen($string);
+            while ($strlen) {
+                $array[] = mb_substr($string, 0, 1, "utf8");
+                $string = mb_substr($string, 1, $strlen, "utf8");
+                $strlen = mb_strlen($string);
+            }
+        }
+        switch ($type) {
+            case 1: {
+                $array = array_reverse($array);
+                for ($i = $bengin; $i < ($bengin + $len); $i++) {
+                    if (isset($array[$i]))
+                        $array[$i] = "*";
+                }
+                $string = implode("", array_reverse($array));
+            }
+                break;
+            case 2: {
+                $array = explode($glue, $string);
+                $array[0] = self::hideStr($array[0], $bengin, $len, 1);
+                $string = implode($glue, $array);
+            }
+                break;
+            case 3: {
+                $array = explode($glue, $string);
+                $array[1] = self::hideStr($array[1], $bengin, $len, 0);
+                $string = implode($glue, $array);
+            }
+                break;
+            case 4: {
+                $left = $bengin;
+                $right = $len;
+                $tem = array();
+                for ($i = 0; $i < ($length - $right); $i++) {
+                    if (isset($array[$i]))
+                        $tem[] = $i >= $left ? "*" : $array[$i];
+                }
+                $array = array_chunk(array_reverse($array), $right);
+                $array = array_reverse($array[0]);
+                for ($i = 0; $i < $right; $i++) {
+                    $tem[] = $array[$i];
+                }
+                $string = implode("", $tem);
+            }
+                break;
+            default: {
+                for ($i = $bengin; $i < ($bengin + $len); $i++) {
+                    if (isset($array[$i]))
+                        $array[$i] = "*";
+                }
+                $string = implode("", $array);
+            }
+                break;
+        }
+        return $string;
+    }
+    /*//if实现的和上面一样的方法
+    public static function hideStr2($string, $bengin=0, $len = 4, $type = 0, $glue = "@") {
+        if (empty($string))
+            return false;
+        $array = array();
+        if ($type == 0 || $type == 1 || $type == 4) {
+            $strlen = $length = mb_strlen($string);
+            while ($strlen) {
+                $array[] = mb_substr($string, 0, 1, "utf8");
+                $string = mb_substr($string, 1, $strlen, "utf8");
+                $strlen = mb_strlen($string);
+            }
+        }
+        if ($type == 0) {
+            for ($i = $bengin; $i < ($bengin + $len); $i++) {
+                if (isset($array[$i]))
+                    $array[$i] = "*";
+            }
+            $string = implode("", $array);
+        }else if ($type == 1) {
+            $array = array_reverse($array);
+            for ($i = $bengin; $i < ($bengin + $len); $i++) {
+                if (isset($array[$i]))
+                    $array[$i] = "*";
+            }
+            $string = implode("", array_reverse($array));
+        }else if ($type == 2) {
+            $array = explode($glue, $string);
+            $array[0] = self::hideStr2($array[0], $bengin, $len, 1);
+            $string = implode($glue, $array);
+        } else if ($type == 3) {
+            $array = explode($glue, $string);
+            $array[1] = self::hideStr2($array[1], $bengin, $len, 0);
+            $string = implode($glue, $array);
+        } else if ($type == 4) {
+            $left = $bengin;
+            $right = $len;
+            $tem = array();
+            for ($i = 0; $i < ($length - $right); $i++) {
+                if (isset($array[$i]))
+                    $tem[] = $i >= $left ? "*" : $array[$i];
+            }
+            $array = array_chunk(array_reverse($array), $right);
+            $array = array_reverse($array[0]);
+            for ($i = 0; $i < $right; $i++) {
+                $tem[] = $array[$i];
+            }
+            $string = implode("", $tem);
+        }
+        return $string;
+    }*/
+
 
 }

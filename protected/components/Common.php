4883ceaf654fb6249fffe30a437464dad2ea7b1e
SallyU <aw133e@163.com>
2016-02-26 15:49:14 +0800

    add

--- protected/components/Common.php
+++ protected/components/Common.php
@@ -1408,6 +1408,15 @@ class Common {
         return $str;
     }
 
+    /*
+     * 删除字符串中的空格
+     */
+    public static function trimAll($str)
+    {
+        $qian=array(" ","　","\t","\n","\r");$hou=array("","","","","");
+        return str_replace($qian,$hou,$str);
+    }
+
     /**
      * 验证手机号是否正确
      * @author Canyian
@@ -1419,7 +1428,7 @@ class Common {
      * 虚拟运营商：170
      */
     public static function isMobile($mobile) {
-        if (!is_numeric($mobile)) {
+        if (!is_numeric(self::trimAll($mobile))) {
             return false;
         }
         return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
@@ -1428,14 +1437,16 @@ class Common {
     /*
      * 生成手机验证码
      */
-    function generate_mobile_code($length = 6) {
+    public static function generate_mobile_code($length = 6) {
         return str_pad(mt_rand(0, pow(10, $length) - 1), $length, '0', STR_PAD_LEFT);
     }
 
-    /* 手机验证码生成函数*/
-    function get_mobile_code($len = 6)
+    /*
+     * 手机验证码生成函数
+     */
+    public static function get_mobile_code($len = 6)
     {
-        $forbidden_num = "1989:10086:12590:1259:10010:10001:10000:";
+        $forbidden_num = "1989:10086:12590:1259:10010:10001:10000:";//去除随机生成的禁止的号码
         do
         {
             //$mobile_code = substr(microtime(), 2, $len);

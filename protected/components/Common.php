a065c28816641cb6cdc98bddde5f6e4793d0a407
SallyU <aw133e@163.com>
2016-03-02 15:20:03 +0800

    增加加密和解密

--- protected/components/Common.php
+++ protected/components/Common.php
@@ -1457,5 +1457,35 @@ class Common {
         return $mobile_code;
     }
 
+    /**
+     * 加密
+     * @param String $string 需要加密的字串
+     * @param String $skey 加密EKY
+     * @author Canyian
+     * @return String
+     */
+    public static function encode($string = '', $skey = 'canyian') {
+        $strArr = str_split(base64_encode($string));
+        $strCount = count($strArr);
+        foreach (str_split($skey) as $key => $value)
+            $key < $strCount && $strArr[$key].=$value;
+        return str_replace(array('=', '+', '/'), array('O0O0O', 'o000o', 'oo00o'), join('', $strArr));
+    }
+
+    /**
+     * 解密
+     * @param String $string 需要解密的字串
+     * @param String $skey 解密KEY
+     * @author Canyian
+     * @return String
+     */
+    public static function decode($string = '', $skey = 'canyian') {
+        $strArr = str_split(str_replace(array('O0O0O', 'o000o', 'oo00o'), array('=', '+', '/'), $string), 2);
+        $strCount = count($strArr);
+        foreach (str_split($skey) as $key => $value)
+            $key <= $strCount  && isset($strArr[$key]) && $strArr[$key][1] === $value && $strArr[$key] = $strArr[$key][0];
+        return base64_decode(join('', $strArr));
+    }
+
 
 }

6275cd68e746e2f52ff4ebb0e8b0394b4a7d0065
SallyU <aw133e@163.com>
2016-06-12 15:50:44 +0800

    add

--- protected/components/Common.php
+++ protected/components/Common.php
@@ -1706,6 +1706,20 @@ class Common {
         return $return;
     }
 
+    public static function cutArticle($data,$cut=0,$str="....")
+    {
+
+        $data=strip_tags($data);//去除html标记
+        $pattern = "/&[a-zA-Z]+;/";//去除特殊符号
+        $data=preg_replace($pattern,'',$data);
+        if(!is_numeric($cut))
+            return $data;
+        if($cut>0)
+            $data=mb_strimwidth($data,0,$cut,$str);
+
+        return $data;
+    }
+
 
 
 
